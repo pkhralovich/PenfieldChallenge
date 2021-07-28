130b3f341f11aaf429d8f476cf7c07971d60109e
Pavel Khralovich <pkhralovich@gmail.com>
2021-07-28 17:24:58 +0200

    Frontend - Implement header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,9 +1,32 @@
 /*External or react libraries*/
 import React from "react";
 
+/* Styles */
+import "./Header.css";
+
+/* Images */
+import nike_icon from "../../assets/images/ic_nike.svg";
+
 function App() {
+    let user = "Pavel";
+
     return (
-        <div> Header </div>
+        <div class="header">
+            <img src={nike_icon} className="icon" />
+            <h1>Nike satisfaction survey</h1>
+
+            <hr className="separator"/>
+
+            <p class="message">
+                Hi {user}! Thank you for your purchase, now you are part of the family! <br/>
+                Please, complete this survey to help us to improve our service. <br/>
+                We will give you a discount voucher after you submit the questions.
+            </p>
+
+            <hr className="separator"/>
+
+            <h2>Let's start!</h2>
+        </div>
     );
 }
 
